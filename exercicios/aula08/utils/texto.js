f25b1e9dfff9ff5ad3fb19fa0c85ae82da874757
Didi-show <gsm22@aluno.ifnmg.edu.br>
2026-04-28 19:21:59 +0000

    Fim

--- exercicios/aula08/utils/texto.js
+++ exercicios/aula08/utils/texto.js
@@ -4,8 +4,5 @@
 // Implemente a função e exporte com "export default".
 export default function formatarNome(nome) {
   // escreva seu código aqui
-  const minusculo = nome.toLowerCase()
-  const primeiraLetra = minusculo.charAt(0).toUpperCase()
-  const resto = minusculo.slice(1)
-  return primeiraLetra + resto
+  return nome.charAt(0).toUpperCase()+nome.slice(1).toLowerCase()
 }
