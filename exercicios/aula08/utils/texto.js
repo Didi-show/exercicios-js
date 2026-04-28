ad4c03e69e43205700cf6ca1102d493272b3a15f
Didi-show <gsm22@aluno.ifnmg.edu.br>
2026-04-28 17:05:19 +0000

    fim

--- exercicios/aula08/utils/texto.js
+++ exercicios/aula08/utils/texto.js
@@ -4,4 +4,11 @@
 // Implemente a função e exporte com "export default".
 export default function formatarNome(nome) {
   // escreva seu código aqui
+  if (typeof nome !== 'string' || nome.length === 0) {
+    return ''
+  }
+  const nomeMinusculo = nome.toLowerCase()
+  const primeiraLetra = nomeMinusculo.charAt(0).toUpperCase()
+  const restoDoNome = nomeMinusculo.slice(1)
+  return primeiraLetra + restoDoNome
 }
